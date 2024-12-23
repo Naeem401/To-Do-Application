b7916594901191f247c63d4f536e85c71a089137
Md Naeem Mia <mdnaeemm401@gmail.com>
2024-12-24 02:18:14 +0300

    Function to create a new to-do-item

--- custom.js
+++ custom.js
@@ -1,3 +1,8 @@
 const todoInput = document.getElementById('todo-input');
 const addToDoBtn = document.getElementById('add-todo-btn');
-const toDoList = document.getElementById('todo-list')
+const toDoList = document.getElementById('todo-list');
+
+function createTodoItem(taskText) {
+    const li = document.createElement('li');
+    li.className = 'flex items-center justify-between p-2 border rounded-md shadow-sm bg-gray-50'
+}
